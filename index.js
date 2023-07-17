a1cfb1ac28d743409f76faa66f462cac11faf3cc
Pete <petr.stepanenko@gmail.com>
2023-07-17 14:40:44 -0400

    make a not-yet working prototype

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser=require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const abbreviationSchema = new mongoose.Schema({
+  originalUrl: String,
+  urlId: Number,
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const Abbreviation = mongoose.model('Abbreviation', abbreviationSchema);
+
+const createNewAbbreviation = (originalUrl, done) => {
+  const createNewAbbreviationId = () => {
+    const newUrlId = Math.floor(Math.random() * 100000);
+    if (!Abbreviation.find({ urlId: newUrlId }) === []) {
+      return createNewAbbreviationId();
+    } return newUrlId;
+  };
+
+  const newAbbreviation = new Abbreviation();
+  newAbbreviation.originalUrl = originalUrl;
+  newAbbreviation.urlId = createNewAbbreviationId;
+
+  newAbbreviation.save((err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  createNewAbbreviation(originalUrl, (err, abbreviation) => {
+    if (err) console.log(err);
+    res.json(abbreviation);
+  });
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
